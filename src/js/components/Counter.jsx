d781c1383f25506cea742319d0b6ddb484980ed2
Alberto Madrid <albertomadrid93@gmail.com>
2025-12-12 22:00:28 +0000

    Terminado con algunos extras

--- src/js/components/Counter.jsx
+++ src/js/components/Counter.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import SecondsCounter from "./SecondsCounter";
 import ClockIcon from "./ClockIcon";
 
-const Counter = ({count, digits}) =>{
+const Counter = ({ count, digits }) => {
     let unidades = count % 10
-    let decenas = (Math.floor(count / 10)%10)
-    let centenas = (Math.floor(count / 100)%10)
-    let millares = (Math.floor(count / 1000)%10)
-    let decenasMillares = (Math.floor(count / 10000)%10)
+    let decenas = (Math.floor(count / 10) % 10)
+    let centenas = (Math.floor(count / 100) % 10)
+    let millares = (Math.floor(count / 1000) % 10)
+    let decenasMillares = (Math.floor(count / 10000) % 10)
 
-    let contador=[]
-    for(let i = 0;i<digits;i++){
-        let newDigit = (Math.floor(count / (10**i))%10)
+    let contador = []
+    for (let i = 0; i < digits; i++) {
+        let newDigit = (Math.floor(count / (10 ** i)) % 10)
         contador.unshift(newDigit)
     }
 
     return (
         <div className="d-flex gap-3 bg-black justify-content-center py-5">
-            <ClockIcon/>
-            {contador.map(x => <SecondsCounter number = {x}/> )}
+            <ClockIcon />
+            {contador.map(x => <SecondsCounter number={x} />)}
         </div>
     )
 }
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,23 +9,19 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
 import Counter from './components/Counter';
 
 let reactRender = ReactDOM.createRoot(document.getElementById('root'))
 
-let decount = 10
+let decount = 100
 let count = 0
 let digits = 5
+let myInterval
 
-let stop =()=>{
-  clearInterval(myInterval);
-}
-let resume = ()=>{
-  let myInterval = setInterval(() => {
+let start = () => {
   reactRender.render(
     <React.StrictMode>
-      <div className='col-6 d-flex justify-content-between mx-auto mt-3'>
+      <div className='col-6 gap-3 d-flex justify-content-between mx-auto mt-3'>
         <button className='btn btn-danger' type="button" onClick={stop}>Parar</button>
         <button className='btn btn-success' type="button" onClick={resume}>Continuar</button>
         <button className='btn btn-warning' type="button" onClick={reset}>Reiniciar</button>
@@ -33,42 +29,22 @@ let resume = ()=>{
       <h1>Contador</h1>
       <Counter count={count} digits={digits} />
       <h1>Cuenta atras</h1>
-
       <Counter count={decount} digits={digits} />
-
     </React.StrictMode>,
   )
   count++
   decount > 0 ? decount-- : null
-
-
-}, 1000);
 }
-let reset = ()=>{
+
+let stop = () => clearInterval(myInterval);
+let resume = () => setInterval(() => start(), 1000);
+let reset = () => {
   count = 0
+  decount = 100
+  start()
 }
 
-let myInterval = setInterval(() => {
-  reactRender.render(
-    <React.StrictMode>
-      <div className='col-6 d-flex justify-content-between mx-auto mt-3'>
-        <button className='btn btn-danger' type="button" onClick={stop}>Parar</button>
-        <button className='btn btn-success' type="button" onClick={resume}>Continuar</button>
-        <button className='btn btn-warning' type="button" onClick={reset}>Reiniciar</button>
-      </div>
-      <h1>Contador</h1>
-      <Counter count={count} digits={digits} />
-      <h1>Cuenta atras</h1>
-
-      <Counter count={decount} digits={digits} />
-
-    </React.StrictMode>,
-  )
-  count++
-  decount > 0 ? decount-- : null
-
-
-}, 1000);
+myInterval = setInterval(() => start(), 1000);
 
 decount == 0 ? alert("Hola desde React!") : null
 
